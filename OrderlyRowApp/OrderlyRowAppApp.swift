46c866ed952bbef34fd6e1a1cbd2e0350c453722
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-14 12:03:36 +0300

    Initial Commit

---
+++ OrderlyRowApp/OrderlyRowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderlyRowAppApp.swift
+//  OrderlyRowApp
+//
+//  Created by jopootrivatel on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct OrderlyRowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
