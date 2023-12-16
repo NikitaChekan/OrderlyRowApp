1f402d7049f252d6f7eccfd5458dd2d02e820451
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-16 15:47:07 +0300

    mission complete

--- OrderlyRowApp/Views/ContentView.swift
+++ OrderlyRowApp/Views/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isPressed: Bool = false
+    let squareCount: Int = 3
+    
     var body: some View {
-        Button (action: buttonAction) {
-            SquareView(squareCount: 10)
+        GeometryReader { geometry in
+            if isPressed {
+                VStack(spacing: 0) {
+                    ForEach(0..<squareCount, id: \.self) { index in
+                        createSquares(index: index, geometry: geometry)
+                    }
+                }
+            } else {
+                HStack(spacing: 3) {
+                    ForEach(0..<squareCount, id: \.self) { index in
+                        createSquares(index: index, geometry: geometry)
+                    }
+                }
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isPressed.toggle()
+            }
         }
     }
     
-    private func buttonAction() {
+    func createSquares(index: Int, geometry: GeometryProxy) -> some View {
         
+        let height = geometry.size.height / CGFloat(squareCount)
+        let rightSpacer = CGFloat(index) * (geometry.size.width - height) / CGFloat(squareCount - 1)
+        let leftSpacer = geometry.size.width - rightSpacer - height
+
+        return HStack {
+            
+            Spacer()
+                .frame(width: isPressed ? leftSpacer : 0)
+            
+            Rectangle()
+                .cornerRadius(15)
+                .foregroundStyle(.blue)
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+                .frame(width: isPressed ? rightSpacer : 0)
+        }
+        .frame(maxHeight: .infinity, alignment: .center)
     }
+    
 }
 
 #Preview {
